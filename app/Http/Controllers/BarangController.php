28c914f70448787efd9c9d7a4341d00957a6e81f
= <nexrey19@gmail.com>
2021-03-30 10:02:13 +0700

    controller fix

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -17,11 +17,14 @@ class BarangController extends Controller
      */
     public function index(Request $request)
     {
+        // ,'kode_barang','katgeori_barang'
         $barang = barang::where([
-            ['nama_barang','!=',Null],
+            ['id_barang','!=',Null],
             [function($query)use($request){
                 if (($term = $request->term)) {
-                    $query->orWhere('nama_barang','LIKE','%'.$term.'%')->get();
+                    $query->orWhere('nama_barang','LIKE','%'.$term.'%')
+                          ->orWhere('kode_barang','LIKE','%'.$term.'%')
+                          ->orWhere('kategori_barang','LIKE','%'.$term.'%')->get();
                 }
             }]
         ])
