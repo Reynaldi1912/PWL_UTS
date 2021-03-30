66533f57eba87bc97dfda26b6e1269b10d2751d9
= <nexrey19@gmail.com>
2021-03-30 08:43:46 +0700

    model fix

--- app/Models/barang.php
+++ app/Models/barang.php
@@ -4,14 +4,13 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Foundation\Auth\Barang as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model; //Model Eloquent
 
 class Mahasiswa extends Model
 {
-    use HasFactory;
-    protected $table = 'barang';
+    protected $table = 'Barang';
     protected $primaryKey = 'id_barang';
 
     protected $fillable = [
