e4360421785e7d76abc2ed0c548ba33f28bb33fd
= <nexrey19@gmail.com>
2021-03-30 08:25:46 +0700

    make model barang

---
+++ app/Models/barang.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloquent
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+    protected $table = 'barang';
+    protected $primaryKey = 'id_barang';
+
+    protected $fillable = [
+        'id_barang',
+        'kode_barang',
+        'nama_barang',
+        'kategori_barang',
+        'harga',
+        'qty',
+        ];
+}
